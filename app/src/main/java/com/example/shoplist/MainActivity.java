67dfbad3230ade61364d61cd7041e76053af6fc2
JakubRoss <Jakub.Rosploch@gmail.com>
2022-06-20 14:55:49 +0200

    clrimports

--- app/src/main/java/com/example/shoplist/MainActivity.java
+++ app/src/main/java/com/example/shoplist/MainActivity.java
@@ -1,13 +1,11 @@
 package com.example.shoplist;
 
-import android.app.Activity;
+
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
